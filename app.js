287d1f8b89ed7501f0e03fbb1dcc2164f7dbd5b0
joeydlee95 <joeydonglee@gmail.com>
2017-11-15 01:20:51 -0800

    features: added chat feature to /chat/ Uses the Socket.IO library to create 2 way communications. Also includes a save feature such that the conversation is saved so that the next time you come back to the room it is there.

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/chat', chat);
+app.use('/chat/', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
